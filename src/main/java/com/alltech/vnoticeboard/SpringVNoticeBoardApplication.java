58b628624e56adb4d1c2c4c9abc294cb7bc0cfe3
shajibsh <shajib.sh@gmail.com>
2016-04-29 16:06:38 -0400

    project codes

---
+++ src/main/java/com/alltech/vnoticeboard/SpringVNoticeBoardApplication.java
@@ -0,0 +1,12 @@
+package com.alltech.vnoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVNoticeBoardApplication.class, args);
+	}
+}
